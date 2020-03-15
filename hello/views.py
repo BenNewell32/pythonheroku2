1f18cc2a29fdf41fac687c4a7af7d46e7978e1fc
Ben Newell <ben.m.newell@gmail.com>
2020-03-14 20:17:35 -0500

    add db

--- hello/views.py
+++ hello/views.py
@@ -3,11 +3,12 @@ from django.http import HttpResponse
 
 from .models import Greeting
 
+import requests
+
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, "index.html")
-
 
 def db(request):
 
